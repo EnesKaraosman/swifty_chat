1681e3b001705898c61f7c333d2a6b68252b8c32
Enes Karaosman <eneskaraosman53@gmail.com>
2021-08-26 11:47:50 +0300

    [FIX] html widget onLinkTap & onImageTap listeners fixed.

--- lib/src/chat-message-list-items/html_widget.dart
+++ lib/src/chat-message-list-items/html_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:styled_widget/styled_widget.dart';
 
+import '../chat.dart';
 import '../models/message.dart';
 
 class HTMLWidget extends StatelessWidget {
@@ -9,23 +10,16 @@ class HTMLWidget extends StatelessWidget {
 
   HTMLWidget({
     required this.chatMessage,
-    // this.onHtmlWidgetPressed
-  }) {
-    // final functions = onHtmlWidgetPressed?.call();
-    // final functions = ChatState onHtmlWidgetPressed?.call();
-    // onLinkTap = functions?["onLinkTap"];
-    // onImageTap = functions?["onImageTap"];
-  }
+  });
 
-  // final Map<String, OnTap> Function()? onHtmlWidgetPressed;
   OnTap? _onLinkTap;
   OnTap? _onImageTap;
 
   @override
   Widget build(BuildContext context) {
-    // Map<String, OnTap> Function()? functions = ChatState.of(context).onHtmlWidgetPressed?.call();
-    // _onLinkTap = functions["onLinkTap"];
-    // _onImageTap = functions["onImageTap"];
+    final functions = ChatStateContainer.of(context).onHtmlWidgetPressed?.call();
+    _onLinkTap = functions?["onLinkTap"];
+    _onImageTap = functions?["onImageTap"];
     return Html(
       data: chatMessage.messageKind.htmlData,
       onLinkTap: _onLinkTap,
